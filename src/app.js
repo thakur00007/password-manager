0163acb15d148e9b026375feb60dff765dedf7f5
thakur00007 <thakurtd01@gmail.com>
2025-02-02 23:18:12 +0530

    Add CORS configuration to support dynamic origin and credentials

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ import express from "express";
 import cors from "cors";
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 app.use(express.json());
 
 import userRouter from "./routes/user.route.js";
