dc4a5a97a6798c51949239c24c9419fcc8875bc8
thakur00007 <thakurtd01@gmail.com>
2025-02-04 23:53:11 +0530

    Add request handler and error handling for undefined routes; set request body limits

--- src/app.js
+++ src/app.js
@@ -6,15 +6,22 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
-app.use(express.json());
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 
 import userRouter from "./routes/user.route.js";
 import passwordRouter from "./routes/password.route.js";
 import questionRouter from "./routes/question.route.js";
+import { requestHandeller } from "./util/requestHandeller.js";
+import { ApiError } from "./util/apiError.js";
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/password", passwordRouter);
 
 app.use("/api/v1/question", questionRouter);
+
+app.use(requestHandeller(async(req, res, next) => {
+    throw new ApiError(404, "Route not found");
+}));
 
 export { app };
